52a0ac28e3c0fb8faa73d3061fab675ec200f695
Janh <josesitoale24@gmail.com>
2026-04-14 17:35:12 -0400

    fix dashboard class: grid-cols-2

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import StatCard from "../Components/StatCard";
 function Dashboard() {
   return (
     // En tu archivo de Página (ej. Home.jsx o Dashboard.jsx)
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
       <StatCard title="Ingresos" value="$23,000" percentage="12%" />
       <StatCard title="Usuarios" value="1,200" percentage="5%" />
       <StatCard title="Ventas" value="450" percentage="8%" />
